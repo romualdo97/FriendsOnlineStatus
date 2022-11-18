5ba4ead653982786739858c14efd8aa5608bc3a3
Romualdo Villalobos <romualdo.villalobos@globant.com>
2022-11-18 10:42:45 -0500

    Added RiderLink

--- Source/FriendsOnlineStatus/Private/View/UIFriendItem.cpp
+++ Source/FriendsOnlineStatus/Private/View/UIFriendItem.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Model/PlayerInfo.h"
 #include "View/UIFriendItem.h"
+#include "Model/PlayerInfo.h"
 #include "Components/TextBlock.h"
 
 void UUIFriendItem::NativeOnListItemObjectSet(UObject* ListItemObject)
